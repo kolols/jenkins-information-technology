441ca0c383e0a8ae9bb00355bf569d9dee405643
kolols <m-plotnik@wp.pl>
2023-11-27 00:33:43 +0100

    fix

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 import app
+import xmlrunner
 
 class TestHello(unittest.TestCase):
 
@@ -25,7 +26,6 @@ class TestHello(unittest.TestCase):
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
-    import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     unittest.main()
